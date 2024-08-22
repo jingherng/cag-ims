7327157b2fdb16e4ae4707920143c66eba24033d
Pow Jing Herng <jingherng2010@gmail.com>
2024-08-23 00:01:32 +0800

    feat: added Item Form and Search component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography } from 'antd';
+import { ItemForm } from './web/components/ItemForm';
+import { SearchComponent } from './web/components/SearchComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { Title } = Typography;
+
+    return (
+        <div className="App">
+            <Title level={2}>Inventory Management System</Title>
+            <ItemForm />
+            <SearchComponent />
+        </div>
+    );
 }
 
 export default App;
