c64d6871803ec75fbdec6cf1c49cb1200d4d4c9a
Pow Jing Herng <jingherng2010@gmail.com>
2024-08-25 15:43:09 +0800

    feat(web): added axios and formtype to itemform to handle on click

--- src/web/components/ItemForm.tsx
+++ src/web/components/ItemForm.tsx
@@ -1,53 +1,75 @@
 import { Button, Card, Form, Input } from "antd";
 import React, { useState } from 'react';
+import axios from 'axios';
+
+type FormType = {
+  name: string;
+  category: string;
+  price: number;
+}
 
 export const ItemForm = (): JSX.Element => {
-    const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
 
-    return (
-        <Card className="bg-slate-300 m-4">
-            <Form layout="vertical">
-                <Form.Item
-                    name="name"
-                    label="Name:"
-                    rules={[{
-                        required: true,
-                        message: "Please enter the item name"
-                    }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    name="category"
-                    label="Category:"
-                    rules={[{
-                        required: true,
-                        message: "Please enter the item category"
-                    }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    name="price"
-                    label="Price:"
-                    rules={[{
-                        required: true,
-                        message: "Please enter the item price"
-                    }]}
-                >
-                    <Input type="number" />
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        className="mt-2 w-full bg-gray-600"
-                        type="primary"
-                        htmlType="submit"
-                        loading={loading}
-                    >
-                        Save Item
-                    </Button>
-                </Form.Item>
-            </Form>
-        </Card>
-    );
+  const handleOnClick = async (values: FormType) => {
+    try {
+      const res = await axios.post('http://localhost:4000/api/add-item', values);
+      console.log(`Item added: ${res.data.id}`);
+      form.resetFields();
+    } catch (err) {
+      console.error(`Error adding item: ${err}`);
+      throw err;
+    }
+  }
+
+  return (
+    <Card className="bg-slate-300 m-4">
+      <Form
+        layout="vertical"
+        onFinish={handleOnClick}
+      >
+        <Form.Item<FormType>
+          name="name"
+          label="Name:"
+          rules={[{
+            required: true,
+            message: "Please enter the item name"
+          }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FormType>
+          name="category"
+          label="Category:"
+          rules={[{
+            required: true,
+            message: "Please enter the item category"
+          }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FormType>
+          name="price"
+          label="Price:"
+          rules={[{
+            required: true,
+            message: "Please enter the item price"
+          }]}
+        >
+          <Input type="number" />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            className="mt-2 w-full bg-gray-600"
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+          >
+            Save Item
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 }
