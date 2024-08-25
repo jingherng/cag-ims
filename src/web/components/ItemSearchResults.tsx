51fcb96137993aa93c02d26de5d70be6d0c3f815
Pow Jing Herng <jingherng2010@gmail.com>
2024-08-25 18:18:08 +0800

    feat(web): added itemSearchResults component for table and filter and sort

---
+++ src/web/components/ItemSearchResults.tsx
@@ -0,0 +1,51 @@
+import { Card, Table, TableColumnsType } from "antd";
+import React from 'react';
+
+export type ItemData = {
+  name: string;
+  price: number;
+  category: string;
+}
+
+type Props = {
+  data: ItemData[];
+}
+
+export const ItemSearchResults = ({
+  data
+}: Props): JSX.Element => {
+  const columns: TableColumnsType<ItemData> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: 'Category',
+      dataIndex: 'category',
+      filters: [...new Map(data.map(item => [item['category'], item])).values()].map((itemData) => ({
+        text: itemData.category,
+        value: itemData.category,
+      })),
+      onFilter: (value, itemData) => itemData.category.includes(value as string)
+    }
+  ];
+
+  const dataSource = data.map((item: ItemData) => ({
+    name: item.name,
+    price: item.price,
+    category: item.category,
+  }));
+
+
+  return (
+    <Card className="bg-slate-300 m-4">
+      <Table columns={columns} dataSource={dataSource} />
+    </Card>
+  );
+}
